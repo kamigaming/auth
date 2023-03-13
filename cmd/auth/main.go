51019bf2f7a2181b9bc4b792838d1864c7d76400
kerelape <kerelape@gmail.com>
2023-03-13 23:19:30 +0500

    Mount apiv1

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	v1 "github.com/kamigaming/auth/internal/app/auth/ui/http/api/v1"
+)
+
+func main() {
+	apiv1 := v1.NewAPIv1()
+	// @todo #2:30m Let users configure the address of the server.
+	// @todo #2:30m Use https instead of http (make it configurable).
+	http.ListenAndServe("localhost:8080", apiv1.Route())
 }
