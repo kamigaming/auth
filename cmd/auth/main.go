80f2d087e6ab6d9bbc449706479e7d478891d40b
kerelape <kerelape@gmail.com>
2023-03-13 23:19:41 +0500

    Mount apiv1

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	v1 "github.com/kamigaming/auth/internal/app/auth/ui/http/api/v1"
 )
 
 func main() {
 	apiv1 := v1.NewAPIv1()
+	router := chi.NewRouter()
+	router.Mount("/api/v1", apiv1.Route())
 	// @todo #2:30m Let users configure the address of the server.
 	// @todo #2:30m Use https instead of http (make it configurable).
-	http.ListenAndServe("localhost:8080", apiv1.Route())
+	http.ListenAndServe("localhost:8080", router)
 }
